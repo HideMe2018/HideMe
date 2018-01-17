9568835c10e8fc93660dbf86440e7a097996838a
HideMe2016 <fengtaihao208@sina.com>
2018-01-17 17:02:19 +0800

    Version 1.0

---
+++ tables/pictureprivacy_relationship.sql
@@ -0,0 +1,43 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win32 (AMD64)
+--
+-- Host: localhost    Database: pictureprivacy
+-- ------------------------------------------------------
+-- Server version	5.7.14-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `relationship`
+--
+
+DROP TABLE IF EXISTS `relationship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `relationship` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(45) NOT NULL,
+  `UserNode` varchar(45) NOT NULL,
+  `FriendName` varchar(45) NOT NULL,
+  `FriendNode` varchar(45) NOT NULL,
+  `Intimacy` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `relationship`
+--
+
+LOCK TABLES `relationship` WRITE;
+/*!40000 ALTER TABLE `relationship` DISABLE KEYS */;
+/*!40000 ALTER TABLE `relationship` ENABLE KEYS */;
+UNLOCK TABLES;
